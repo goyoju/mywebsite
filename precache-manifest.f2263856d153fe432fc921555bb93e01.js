3b584ee13830e33c10eb0e70ca2202c1d4c378c5
goyoju <junyoung020725@gmail.com>
2025-04-18 04:14:27 -0400

    Updates

---
+++ precache-manifest.f2263856d153fe432fc921555bb93e01.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3875fb5825c4038b92f20bc28870328d",
+    "url": "/goyoju/mywebsite.git/index.html"
+  },
+  {
+    "revision": "36e6d42a9dee7508b083",
+    "url": "/goyoju/mywebsite.git/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "6593f894d016e85388cb",
+    "url": "/goyoju/mywebsite.git/static/js/2.c7821bdd.chunk.js"
+  },
+  {
+    "revision": "7373c6e4eb6af1a9d439",
+    "url": "/goyoju/mywebsite.git/static/js/3.ba8b7f13.chunk.js"
+  },
+  {
+    "revision": "36e6d42a9dee7508b083",
+    "url": "/goyoju/mywebsite.git/static/js/main.884a2185.chunk.js"
+  },
+  {
+    "revision": "a185b7e6bb1907e5d30f",
+    "url": "/goyoju/mywebsite.git/static/js/runtime~main.141e4e32.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/goyoju/mywebsite.git/static/media/logo.06e73328.svg"
+  }
+]);
